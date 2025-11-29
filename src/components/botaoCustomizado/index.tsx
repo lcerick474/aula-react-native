315ae40f710bb463b524c5529a901b0fba7d05cf
Daniele <danielemarquesripardo@gmail.com>
2025-11-28 21:01:02 -0300

    feature: bordas caixa de texto

--- src/components/botaoCustomizado/index.tsx
+++ src/components/botaoCustomizado/index.tsx
@@ -2,7 +2,7 @@ import { Button, ButtonProps, TextInputProps, View } from 'react-native';
 
 export function BotaoCustomizado(rest: ButtonProps) {
   return (
-    <View className="rounded-2x1 w-full border-2 border-black-500">
+    <View className="rounded-2x1 border-black-500 w-full border-2">
       <Button {...rest} />
     </View>
   );
