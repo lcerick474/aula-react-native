297a3df64b1e81f453a088b27d0ebd537fa65a4d
lucas <lucaserickmendestavares@gmail.com>
2025-12-02 20:46:54 -0300

    feature: configurações de campo de texto e senha mais add do botão

--- src/screens/login.tsx
+++ src/screens/login.tsx
@@ -1,16 +1,27 @@
 import { BotaoCustomizado } from '@/components/botaoCustomizado';
 import { InputCustomizado } from '@/components/inputCustomizado';
+import { useState } from 'react';
 import { Text, View } from 'react-native';
 
 export function LoginScreen() {
+  const [login, setLogin] = useState('');
+  const [senha, setSenha] = useState('');
+
   return (
     <View className="flex-1 items-center justify-center">
       <View className="w-full p-5 gap-5 justify-center items-center">
         <Text className="text-3xl">Nosso app</Text>
 
-      <InputCustomizado placeholder={'Login'} />
-      <InputCustomizado placeholder="senha" />
-      <BotaoCustomizado title="entrar" />
+      <InputCustomizado placeholder={'Login'}
+      onChangeText={(text) => {setLogin(text)}}
+      />
+      <InputCustomizado placeholder="senha"
+      onChangeText={(text) => {setSenha(text)}}
+      secureTextEntry={true}
+      />
+      <BotaoCustomizado title="entrar"
+      onPress={() => console.log("Login:", login, "Senha:", senha)}
+      />
       <Text className="text-blue-500">Esqueceu a senha</Text>
 
       </View>
